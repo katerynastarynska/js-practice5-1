9c6dab28f65301c20e199634de563510fe4bf944
katerynastarynska <katerynastarynska@gmail.com>
2022-10-02 11:27:56 -0500

    markup and render

---
+++ index.js
@@ -0,0 +1,78 @@
+import teachers from "./teachers.js";
+import { students } from "./students.js";
+
+const teacherRow = document.querySelector(".row");
+
+const teacherCard = (teacher) => {
+    return `
+    <div class="col mb-3 col-md-4">
+        <div class="card">
+            <img src="${teacher.profilePicUrl}" alt="Teacher image" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${teacher.topic}</h5>
+                    <p class="card-text">${teacher.desc}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Rate: ${teacher.rate} ${teacher.rateUnit}</li>
+                    <li class="list-group-item">Location: ${teacher.location}</li>
+                </ul>
+                <div class="card-body">
+                    <a href class="card-link">Contact</a>
+                    <a href class="card-link">Book a session</a>
+                </div>
+        </div>
+    </div>` 
+}
+
+teachers.map((teacher) => {
+    teacherRow.insertAdjacentHTML('beforeend', teacherCard(teacher))
+})
+
+const renderStudent = (student) => {
+    const studentCol = createStudentCard(student);
+    const studentRow = document.getElementById('student-row');
+    studentRow.appendChild(studentCol);
+}
+
+const createStudentCard = (student) => {
+    const col = document.createElement('div');
+    col.className = 'col-md-3';
+    const card = document.createElement('div')
+    card.className = 'card';
+    const cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header';
+    cardHeader.innerHTML = student.name;
+
+    const cardBody = document.createElement('div')
+    cardBody.className = 'card-body';
+    const title = document.createElement('h5')
+    title.className = 'card-title'
+    title.innerHTML = student.title;
+    const cardText = document.createElement('p')
+    cardText.className = 'card-text'
+    cardText.innerHTML = student.desc;
+
+    const linkButton = document.createElement('a');
+    const btn = document.createElement('button');
+    btn.className = 'btn btn-primary';
+    btn.innerHTML = 'Connect';
+    linkButton.appendChild(btn);
+
+    const footer = document.createElement('div');
+    footer.className = 'card-footer';
+    footer.innerHTML = student.joindedDate;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(linkButton);
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    card.appendChild(footer);
+
+    return col;
+}
+
+for (let i = 0; i < students.length; i += 1) {
+    renderStudent(students[i]);
+}
---
+++ students.js
@@ -0,0 +1,44 @@
+export const students = [
+    {
+        name: 'John Doe',
+        title: 'Math Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 28, 2022'),
+    },
+    {
+        name: 'John Smith',
+        title: 'Science Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 18, 2022'),
+    },
+    {
+        name: 'Anna Len',
+        title: 'Physics Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 1, 2022'),
+    },
+    {
+        name: 'Logan Kit',
+        title: 'Computer Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 11, 2022'),
+    },
+    {
+        name: 'Emily Rey',
+        title: 'Math Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 21, 2022'),
+    },
+    {
+        name: 'Donna M',
+        title: 'English Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 9, 2022'),
+    },
+    {
+        name: 'Eric Smith',
+        title: 'Spelling Student',
+        desc: 'Lorem ipsum dolor sit amet, consectetur',
+        joindedDate: new Date('September 14, 2022'),
+    },
+]
